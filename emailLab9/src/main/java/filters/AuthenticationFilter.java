9909b387dedeb093201cd9a708a0d3508a88596c
imneil04 <97461255+imneil04@users.noreply.github.com>
2022-04-10 19:55:03 -0600

    Initial Commit
    Login Page user can login
    
    Admin page admin can login
    
    Link to forgot password
    
    Validation to making sure proper email is entered
    
    Back to login page link once reset is done

---
+++ emailLab9/src/main/java/filters/AuthenticationFilter.java
@@ -0,0 +1,60 @@
+package filters;
+
+import java.io.IOException;
+//import java.io.PrintStream;
+//import java.io.PrintWriter;
+//import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class AuthenticationFilter implements Filter {
+    
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+
+            // code that is executed before the servlet
+            HttpServletRequest httpRequest = (HttpServletRequest)request;
+            HttpSession session = httpRequest.getSession();
+            String email = (String)session.getAttribute("email");
+            String password = (String)session.getAttribute("password");
+            
+            if (email == null || password.isEmpty()) {
+              //  HttpServletResponse httpResponse = (HttpServletResponse)response;
+                request.setAttribute("message", "email address and/or password is missing. Please try again. ");
+              //  httpResponse.sendRedirect("login");
+                request.getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);  
+                return;
+            }
+            else {
+                 chain.doFilter(request, response); // execute the servlet
+                 return;
+            }
+             
+            
+            
+       
+            
+            // code that is executed after the servlet
+            
+        
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        
+    }
+
+    @Override
+    public void destroy() {
+       
+    }
+}
